da283fe1a7b15daae1e77371a347eba220673765
rroyyyy <93365944+rroyyyy@users.noreply.github.com>
2022-06-30 03:42:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,18 @@ import time
 # ==================================
 # Global Settings
 # Github Action
-enable_gh_action = False  # 启用GitHub Action
+enable_gh_action = True  # 启用GitHub Action
 # Bark Push
-enable_bark = False  # 启用Bark推送
-bark_token = ""  # Bark推送ID
+enable_bark = True  # 启用Bark推送
+bark_token = "dKoJXqwVzKhSY7QngdE9gF"  # Bark推送ID
 # Serverchan Push
 enable_serverchan = False  # 启用Server酱(Turbo)推送
 sct_token = ""  # Sendkey
 # Ticket Stuff
 startSite = "SK"  # 始发站点
 endSite = "HKA"  # 目标站点
-startDate = "2022-04-20"  # 船票搜索日期
-endDate = "2022-04-30"
+startDate = "2022-07-30"  # 船票搜索日期
+endDate = "2022-09-16"
 show_available_only = True  # 只显示有票的日期
 # ==================================
 
